6d71f7656c42ed6363abebca37c40adee55bc1ec
Michael Afari <michealnewtons419@gmail.com>
2024-05-27 12:36:47 +0200

    navbar

---
+++ src/components/Navbar.vue
@@ -0,0 +1,28 @@
+<template>
+    <v-toolbar class="test">
+        <v-toolbar-title>Créateur d'équipe Pokémon</v-toolbar-title>
+        <v-toolbar-items>
+            <v-btn to="/">Home</v-btn>
+            <v-btn to="/creation-team">Créer une équipe</v-btn>
+            <v-btn to="/team">Mon équipe</v-btn>
+        </v-toolbar-items>
+    </v-toolbar>
+</template>
+
+<script>
+export default {
+    name: 'NavBar'
+}
+</script>
+
+<style scoped>
+nav {
+    display: flex;
+    gap: 1rem;
+}
+
+.test {
+    background-color: #0a141e;
+    color: yellow;
+}
+</style>
