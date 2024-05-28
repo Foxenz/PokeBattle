dba23df3efa76771b8a2551b63a1919d89178326
Fred <frederic.vanovermeire@gmail.com>
2024-05-28 22:41:58 +0200

    add paginiation to list of all pokemon

--- src/components/ListOfAllPokemon.vue
+++ src/components/ListOfAllPokemon.vue
@@ -16,9 +16,14 @@
     </div>
 
     <div class="list-container">
-      <div class="pokemon-info" v-for="pokemon in filteredPokemons" :key="pokemon.id">
+      <div class="pokemon-info" v-for="pokemon in filteredPokemons" :key="pokemon.name">
         <PokemonCard :pokemon="pokemon" />
       </div>
+    </div>
+
+    <div class="pagination">
+      <button @click="prevPage" :disabled="offset === 0">Previous</button>
+      <button @click="nextPage">Next</button>
     </div>
   </section>
 </template>
@@ -34,13 +39,15 @@ export default {
   data() {
     return {
       pokemons: [],
-      filteredPokemons: []
+      filteredPokemons: [],
+      offset: 0,
+      limit: 24,
     }
   },
 
   methods: {
     async loadAllPokemons() {
-      this.pokemons = await apiHandler.fetchAllPokemonWithPictures()
+      this.pokemons = await apiHandler.fetchPokemonList(this.offset, this.limit);
       this.filteredPokemons = this.pokemons
     },
 
@@ -51,6 +58,18 @@ export default {
         this.filteredPokemons = this.pokemons.filter((pokemon) => {
           return pokemon.name.toLowerCase().includes(search.toLowerCase())
         })
+      }
+    },
+
+    async nextPage() {
+      this.offset += this.limit;
+      await this.loadAllPokemons();
+    },
+
+    async prevPage() {
+      if (this.offset >= this.limit) {
+        this.offset -= this.limit;
+        await this.loadAllPokemons();
       }
     }
   },
@@ -84,7 +103,29 @@ section {
     overflow: auto;
     width: 100%;
   }
+
+  .pagination {
+    display: flex;
+    justify-content: center;
+    margin-top: 20px;
+
+    button {
+      background-color: #0a141e;
+      border: 1px solid white;
+      border-radius: 5px;
+      color: white;
+      cursor: pointer;
+      margin: 0 10px;
+      padding: 10px 20px;
+
+      &:disabled {
+        cursor: not-allowed;
+        opacity: 0.5;
+      }
+    }
+  }
 }
+
 ::-webkit-scrollbar {
   width: 10px;
 }
@@ -101,3 +142,4 @@ section {
   cursor: pointer;
 }
 </style>
+
