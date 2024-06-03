380e9b0c15cbd2cec1b8fce926163b79e15d4e6a
Fred <frederic.vanovermeire@gmail.com>
2024-06-03 19:49:19 +0200

    team's best stats done

--- src/views/TeamView.vue
+++ src/views/TeamView.vue
@@ -1,27 +1,54 @@
 <template>
-  <NavBar />
-  <main>
-    <div class="fixed-component">
-      <UserTeam class="user-team" />
-    </div>
-  </main>
+    <NavBar />
+    <main>
+        <div class="fixed-component">
+            <UserTeam class="user-team" />
+        </div>
+        <div class="flexible-component">
+            <TeamsBestStats />
+        </div>
+    </main>
 </template>
 
 <script>
 import NavBar from '@/components/NavBar.vue'
 import UserTeam from '@/components/UserTeam.vue'
+import TeamsBestStats from '@/components/TeamsBestStats.vue'
+
 export default {
-  name: 'TeamView',
+    name: 'TeamView',
 
-  data() {
-    return {
-      title: 'Team page'
+    data() {
+        return {
+            title: 'Team page'
+        }
+    },
+
+    components: {
+        UserTeam,
+        NavBar,
+        TeamsBestStats
     }
-  },
-
-  components: {
-    UserTeam,
-    NavBar
-  }
 }
 </script>
+
+<style scoped>
+main {
+    height: calc(100% - 64px);
+    display: flex;
+    align-items: stretch;
+}
+
+.fixed-component {
+    flex: 0 0 auto;
+    display: flex;
+    align-items: center;
+}
+
+.flexible-component {
+    flex: 1;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+</style>
