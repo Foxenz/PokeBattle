fbdde3d44382bb9a735e798e3d4a6501a1c30374
Michael Afari <michealnewtons419@gmail.com>
2024-06-04 12:35:43 +0200

    Worked on baseStat Progress bar

--- src/views/InfoPokemonView.vue
+++ src/views/InfoPokemonView.vue
@@ -7,7 +7,7 @@
           <!-- IMAGE INFO -->
           <v-col cols="12" md="6" class="bg-blue-grey-darken-2 mb-1">
             <v-card class="ma-2 pa-2 bg-blue-grey-darken-1">
-              <v-img :src="pokemonInfo.sprites.front_default" :height="395" cover class="bg-blue-grey-darken-4">
+              <v-img :src="pokemonInfo.sprites.front_default" cover class="bg-blue-grey-darken-4">
                 <template v-slot:placeholder>
                   <div class="d-flex align-center justify-center fill-height">
                     <v-progress-circular color="grey-lighten-4" indeterminate></v-progress-circular>
@@ -18,7 +18,7 @@
           </v-col>
           <!-- Attributes INFO -->
           <v-col cols="12" md="6" class="bg-blue-grey-darken-2 mb-1">
-            <v-card class="ma-1 pa-2 bg-yellow-accent-4">
+            <v-card class="ma-3 pa-2 bg-yellow-accent-4" :height="378">
               <v-card-title>
                 <p class="text-h4 font-weight-bold text-uppercase text-center font">{{ pokemonInfo.name }}</p>
               </v-card-title>
@@ -26,7 +26,7 @@
                 <v-card-title>
                   <p class="text-h6 font-weight-bold text-yellow-accent-4">Abilities</p>
                   <p v-for="ability in pokemonInfo.abilities" :key="ability.ability.name"
-                    class="text-capitalize text-subtitle-1">
+                    class="text-capitalize text-caption">
                     {{
                       ability.ability.name }}</p>
                 </v-card-title>
@@ -36,8 +36,8 @@
                 <v-card-title>
                   <p class="text-h6 font-weight-bold text-yellow-accent-4">Physic</p>
                   <v-sheet class="bg-blue-grey-darken-4">
-                    <p class="font-weight-regular text-subtitle-1">Height: {{ pokemonInfo.height }} in</p>
-                    <p class="font-weight-regular text-subtitle-1">Weight: {{ pokemonInfo.weight }} kg</p>
+                    <p class="font-weight-regular text-caption">Height: {{ pokemonInfo.height }} in</p>
+                    <p class="font-weight-regular text-caption">Weight: {{ pokemonInfo.weight }} kg</p>
                   </v-sheet>
                 </v-card-title>
               </v-card>
@@ -56,12 +56,13 @@
             <v-card class="my-2 bg-blue-grey-darken-4">
               <v-card-title>
                 <p class="text-h6 text-center font-weight-bold">BASE STATS</p>
-                <v-sheet class="bg-blue-grey-darken-4" v-for="stat in pokemonInfo.stats" :key="stat.stat.name">
-                  <p class="text-caption text-capitalize"> {{ stat.stat.name }}</p>
-                  <v-progress-linear v-model="stat.base_stat" color="amber" height="25" class="my-1">
-                    <strong>{{ Math.ceil(stat.base_stat) }}%</strong>
+                <v-sheet class="bg-blue-grey-darken-4" v-for="stat in statPercentages" :key="stat.name">
+                  <p class="text-caption text-capitalize">{{ stat.name }}</p>
+                  <v-progress-linear v-model="stat.percentage" color="amber" height="25" class="my-1">
+                    <strong>{{ Math.ceil(stat.baseFigure) }}</strong>
                   </v-progress-linear>
                 </v-sheet>
+
               </v-card-title>
             </v-card>
           </v-col>
@@ -82,6 +83,7 @@ export default {
   name: 'InfoPokemonView',
   data() {
     return {
+
       idPokemon: this.$route.params.id,
       pokemonInfo: {
         name: '',
@@ -107,6 +109,17 @@ export default {
         }
       }
     };
+  },
+  computed: {
+    statPercentages() {
+      return this.pokemonInfo.stats.map(stat => {
+        return {
+          name: stat.stat.name,
+          baseFigure: stat.base_stat,
+          percentage: (stat.base_stat / 255) * 100
+        };
+      });
+    }
   },
   methods: {
     async fetchPokemonData(idPokemon) {
