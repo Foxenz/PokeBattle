7b5f69bc14c8dc7cdebee3cabc70f6e159054afc
Alexandre <102592431+Foxenz@users.noreply.github.com>
2024-06-11 19:11:45 +0200

    Changement de l'image

--- src/views/InfoPokemonView.vue
+++ src/views/InfoPokemonView.vue
@@ -7,7 +7,11 @@
           <!-- IMAGE INFO -->
           <v-col cols="12" md="6" class="bg-blue-grey-darken-2 mb-1">
             <v-card class="ma-2 pa-2 bg-blue-grey-darken-1">
-              <v-img :src="pokemonInfo.sprites.front_default" cover class="bg-blue-grey-darken-4">
+              <v-img
+                :src="pokemonInfo.sprites.other['official-artwork'].front_default"
+                cover
+                class="bg-blue-grey-darken-4"
+              >
                 <template v-slot:placeholder>
                   <div class="d-flex align-center justify-center fill-height">
                     <v-progress-circular color="grey-lighten-4" indeterminate></v-progress-circular>
@@ -20,15 +24,20 @@
           <v-col cols="12" md="6" class="bg-blue-grey-darken-2 mb-1">
             <v-card class="ma-3 pa-2 bg-yellow-accent-4" :height="378">
               <v-card-title>
-                <p class="text-h4 font-weight-bold text-uppercase text-center font">{{ pokemonInfo.name }}</p>
+                <p class="text-h4 font-weight-bold text-uppercase text-center font">
+                  {{ pokemonInfo.name }}
+                </p>
               </v-card-title>
               <v-card class="my-1 bg-blue-grey-darken-4">
                 <v-card-title>
                   <p class="text-h6 font-weight-bold text-yellow-accent-4">Abilities</p>
-                  <p v-for="ability in pokemonInfo.abilities" :key="ability.ability.name"
-                    class="text-capitalize text-caption">
-                    {{
-                      ability.ability.name }}</p>
+                  <p
+                    v-for="ability in pokemonInfo.abilities"
+                    :key="ability.ability.name"
+                    class="text-capitalize text-caption"
+                  >
+                    {{ ability.ability.name }}
+                  </p>
                 </v-card-title>
               </v-card>
 
@@ -36,15 +45,24 @@
                 <v-card-title>
                   <p class="text-h6 font-weight-bold text-yellow-accent-4">Physic</p>
                   <v-sheet class="bg-blue-grey-darken-4">
-                    <p class="font-weight-regular text-caption">Height: {{ pokemonInfo.height }} in</p>
-                    <p class="font-weight-regular text-caption">Weight: {{ pokemonInfo.weight }} kg</p>
+                    <p class="font-weight-regular text-caption">
+                      Height: {{ pokemonInfo.height }} in
+                    </p>
+                    <p class="font-weight-regular text-caption">
+                      Weight: {{ pokemonInfo.weight }} kg
+                    </p>
                   </v-sheet>
                 </v-card-title>
               </v-card>
               <v-card class="my-2 bg-blue-grey-darken-4">
                 <v-card-title>
                   <p class="text-h6 font-weight-bold text-yellow-accent-4">Type</p>
-                  <v-card class="type" :class="type.type.name" v-for="type in pokemonInfo.types" :key="type">
+                  <v-card
+                    class="type"
+                    :class="type.type.name"
+                    v-for="type in pokemonInfo.types"
+                    :key="type"
+                  >
                     {{ type.type.name }}
                   </v-card>
                 </v-card-title>
@@ -56,34 +74,39 @@
             <v-card class="my-2 bg-blue-grey-darken-4">
               <v-card-title>
                 <p class="text-h6 text-center font-weight-bold">BASE STATS</p>
-                <v-sheet class="bg-blue-grey-darken-4" v-for="stat in statPercentages" :key="stat.name">
+                <v-sheet
+                  class="bg-blue-grey-darken-4"
+                  v-for="stat in statPercentages"
+                  :key="stat.name"
+                >
                   <p class="text-caption text-capitalize">{{ stat.name }}</p>
-                  <v-progress-linear v-model="stat.percentage" color="amber" height="25" class="my-1">
+                  <v-progress-linear
+                    v-model="stat.percentage"
+                    color="amber"
+                    height="25"
+                    class="my-1"
+                  >
                     <strong>{{ Math.ceil(stat.baseFigure) }}</strong>
                   </v-progress-linear>
                 </v-sheet>
-
               </v-card-title>
             </v-card>
           </v-col>
         </v-row>
       </v-container>
-
-
-
     </v-container>
   </div>
 </template>
 
 <script>
-import NavBar from '@/components/NavBar.vue';
-import ApiHandler from '@/services/ApiHandler';
+import NavBar from '@/components/NavBar.vue'
+import ApiHandler from '@/services/ApiHandler'
+import PokemonCard from '@/components/PokemonCard.vue'
 
 export default {
   name: 'InfoPokemonView',
   data() {
     return {
-
       idPokemon: this.$route.params.id,
       pokemonInfo: {
         name: '',
@@ -105,40 +128,45 @@ export default {
           }
         ],
         sprites: {
-          front_default: ''
+          other: {
+            'official-artwork': {
+              front_default: ''
+            }
+          }
         }
       }
-    };
+    }
   },
   computed: {
     statPercentages() {
-      return this.pokemonInfo.stats.map(stat => {
+      return this.pokemonInfo.stats.map((stat) => {
         return {
           name: stat.stat.name,
           baseFigure: stat.base_stat,
           percentage: (stat.base_stat / 255) * 100
-        };
-      });
+        }
+      })
     }
   },
   methods: {
     async fetchPokemonData(idPokemon) {
       try {
-        const data = await ApiHandler.fetchPokemon(idPokemon);
-        this.pokemonInfo = data;
-        console.log(this.pokemonInfo);
+        const data = await ApiHandler.fetchPokemon(idPokemon)
+        this.pokemonInfo = data
+        console.log(this.pokemonInfo)
       } catch (error) {
-        console.error('Error fetching Pokemon data:', error);
+        console.error('Error fetching Pokemon data:', error)
       }
     }
   },
   created() {
-    this.fetchPokemonData(this.idPokemon);
+    this.fetchPokemonData(this.idPokemon)
   },
   components: {
+    PokemonCard,
     NavBar
   }
-};
+}
 </script>
 
 <style scoped>
